4962de8e5bdd5f3b207c17e98cf80200e30399d8
Student User <student@workstation.lab.example.com>
2021-05-18 01:26:00 -0400

    updated index.php - 4th try

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is in the air \n"
-print "Good things will come to those who waint. \n";
+print "A change is in the air for sure! \n"
 ?>
